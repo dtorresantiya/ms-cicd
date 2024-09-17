3f36edcd370d4748b43e8f1e281535707eb29777
Deivis Torres Mena <babyyoda62406@gmail.com>
2024-09-17 12:11:13 -0400

    A litle change

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.PORT);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  await app.listen(envs.PORT, '0.0.0.0');
   console.log(`Server is running on ${envs.PORT} port`);
   
 }
